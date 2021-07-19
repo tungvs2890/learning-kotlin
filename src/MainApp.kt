6f65b3b705b7441dc1e477dc64aa1e2897537961
tungvuong <vuongtung0208@gmail.com>
2021-07-19 23:54:16 +0700

    new app file

---
+++ src/MainApp.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var helloString: String = "Hello World"
+    var address: String = """
+số 24 đường 7, khu phố X
+phường 5, Quận 9
+TP.HCM
+    """
+    val x:Int=1
+
+    var arg:IntArray = intArrayOf(1,2,5,7)
+    print(helloString+address)
+}
